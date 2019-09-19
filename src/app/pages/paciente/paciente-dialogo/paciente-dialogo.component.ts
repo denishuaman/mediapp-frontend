598d2fba8152047ae2bb3c97e3a399d0c8607231
Denis <denis07.1993@gmail.com>
2019-09-18 23:08:56 -0500

    Se agregó la validación del formulario del componente paciente-dialogo.

--- src/app/pages/paciente/paciente-dialogo/paciente-dialogo.component.ts
+++ src/app/pages/paciente/paciente-dialogo/paciente-dialogo.component.ts
@@ -3,6 +3,7 @@ import { Paciente } from 'src/app/_model/paciente';
 import { MatDialogRef } from '@angular/material';
 import { PacienteService } from 'src/app/_service/paciente.service';
 import { switchMap } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-paciente-dialogo',
@@ -13,12 +14,21 @@ export class PacienteDialogoComponent implements OnInit {
 
   paciente: Paciente;
   nuevoPacienteRegistrado: Paciente;
+  form: FormGroup;
 
   constructor(private dialogRef: MatDialogRef<PacienteDialogoComponent>, 
     private pacienteService: PacienteService) { }
 
   ngOnInit() {
     this.paciente = new Paciente();
+    this.form = new FormGroup({
+      'nombres': new FormControl('', [Validators.minLength(3), Validators.maxLength(70)]),
+      'apellidos': new FormControl('', [Validators.minLength(3), Validators.maxLength(70)]),
+      'email': new FormControl('', [Validators.email, Validators.maxLength(55)]),
+      'telefono': new FormControl('', [Validators.minLength(9), Validators.maxLength(9), Validators.pattern(/^-?(0|[0-9]\d*)?$/)]),
+      'dni': new FormControl('', [Validators.minLength(8), Validators.maxLength(8), Validators.pattern(/^-?(0|[0-9]\d*)?$/)]),
+      'direccion': new FormControl('', [Validators.minLength(3), Validators.maxLength(150)])
+    });
   }
 
   registrar() {
