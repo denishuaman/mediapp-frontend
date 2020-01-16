4013914dde70b7f9a87d5b01f77ee107ea473bb8
Denis Huamán Acuña <dhuamana@reniec.gob.pe>
2020-01-16 11:16:04 -0500

    Se agregó funcionalidad para exportar la lista de pacientes

--- src/app/pages/paciente/paciente.component.ts
+++ src/app/pages/paciente/paciente.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Paciente } from 'src/app/_model/paciente';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, MatPaginator, MatSnackBar } from '@angular/material';
+import { ExcelService } from '../../_service/excel.service';
 
 @Component({
   selector: 'app-paciente',
@@ -17,7 +18,8 @@ export class PacienteComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   cantidad: number = 0;
 
-  constructor(private pacienteService: PacienteService, private snackBar: MatSnackBar) { }
+  constructor(private pacienteService: PacienteService, private snackBar: MatSnackBar,
+    private excelService: ExcelService) { }
 
   ngOnInit() {
 
@@ -48,7 +50,7 @@ export class PacienteComponent implements OnInit {
     });
   }
 
-  mostrarMas(e : any){
+  mostrarMas(e: any) {
     //console.log(e);
     this.pacienteService.listarPageable(e.pageIndex, e.pageSize).subscribe(data => {
       this.cantidad = data.totalElements;
@@ -71,4 +73,22 @@ export class PacienteComponent implements OnInit {
     });
   }
 
+  descargarReporteExcel() {
+    let lista = new Array<any>();
+    this.pacienteService.listar().subscribe((pacientes) => {
+      pacientes.forEach((p) => {
+        let pac = {
+          'Código': p.idPaciente,
+          'Nombres': p.nombres,
+          'Apellidos': p.apellidos,
+          'DNI': p.dni,
+          'Dirección': p.direccion,
+          'Teléfono': p.telefono,
+          'E-mail': p.email
+        }
+        lista.push(pac);
+      });
+      this.excelService.exportAsExcelFile(lista, 'lista_pacientes');
+    })
+  }
 }
